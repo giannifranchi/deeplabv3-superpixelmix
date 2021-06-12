d66cad9ddd6d8e90c8a73cdec14d9294d1876111
giannifranchi <gianni.franchi@ensta-paris.fr>
2021-06-12 14:53:03 +0200

    lPBNN

--- main_LPBNN.py
+++ main_LPBNN.py
@@ -59,8 +59,8 @@ def get_argparser():
                         help='crop validation (default: False)')
     parser.add_argument("--batch_size", type=int, default=16,
                         help='batch size (default: 16)')
-    parser.add_argument("--val_batch_size", type=int, default=4,
-                        help='batch size for validation (default: 4)')
+    parser.add_argument("--val_batch_size", type=int, default=1,
+                        help='batch size for validation (default: 1)')
     parser.add_argument("--crop_size", type=int, default=513)
     
     parser.add_argument("--ckpt", default=None, type=str,
